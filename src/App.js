d4fd6afb36e403a895c639dee57dfce0d8f55542
carpek <77640624+carpek@users.noreply.github.com>
2021-05-02 14:38:13 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-
+  <div className="container">
 
      <h1>Weather App</h1>
 
@@ -12,6 +12,7 @@ export default function App() {
 <footer>This weather app was codes by
 <a href="https://github.com/carpek/Weather-react-app" target="_blank">  Lina Pek </a>
 and is open sourced.</footer>
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 ReactDOM.render(
   <React.StrictMode>
