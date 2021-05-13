6d41a3ed03c3c5b1daccbd008ff0eedcd7442730
carpek <77640624+carpek@users.noreply.github.com>
2021-05-13 13:05:11 +0200

    added forecast loop

--- src/ForecastDaily.js
+++ src/ForecastDaily.js
@@ -8,38 +8,40 @@ export default function ForecastDaily(props) {
 let [loaded, setLoaded] = useState(false);
 let [forecast, setForecast] = useState (null);
 
+
 function handleResponse(response) {
 setForecast(response.data.daily);
 setLoaded(true);
 }
+
 if (loaded) {
     return (
         <div className="ForecastDaily">
         <div className="row">
-        <div className="col"> 
-       <ForecastDay data={forecast[0]} />
+            {forecast.map(function(dailyForecast, index) {
+            if (index < 6) {
+            return (
+            <div className="col" key={index}> 
+            <ForecastDay data={dailyForecast} /> 
+            </div>
+            );
+        }
+        })}
         
-        </div>
-        </div>
-        </div>
-        
-        
-        );
-
-
-
-
+</div>
+</div>
+);
 
 } else {
-    let apiKey="a8b18d5e574b14c1d3de44331ec7e970";
+
+let apiKey="a8b18d5e574b14c1d3de44331ec7e970";
 let longitude = props.coordinates.lon;
 let latitude = props.coordinates.lat;
-    let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(handleResponse);
 
 return null;
-
 }
 
 }
--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,20 +1,41 @@
 import React from "react";
 
 export default function ForecastDay(props) {
+function maxTemp() {
+let temperature = Math.round(props.data.temp.max);
+return `${temperature} °C`;
+}
+
+function minTemp() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature} °C`;
+    }
+
+function day() {
+
+    let date = new Date(props.data.dt *1000);
+    let day = date.getDay();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+
+}
+
 return (
     <div>
     <div classsName="ForecastDaily-day">
-    {props[0].dt} </div>
+    {day()} </div>
     
     <img alt="Weathericon" src={props[0].weather[0].icon} size={32}></img> 
     
     <div className="ForecastDaily-temp">
     <span className="ForecastDaily-temp-max">
-    {Math.round(props[0].temp.max)}°C 
+    {maxTemp()} 
     </span>
     
     <span className="ForecastDaily-temp-min">
-    {Math.round(props[0].temp.min)}°C
+    {minTemp()}
     </span>
     </div>
     </div>
